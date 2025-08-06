3d63547c13d8d32e961e1e64070fd565eb4fd00c
Manoel Vicente <manoelj.vicente@gmail.com>
2025-08-06 20:14:33 -0300

    Create function addFriend and clearField

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let friends = [];
+
+function addFriend() {
+    let getFriend = document.getElementById("friend").value;
+
+    if (getFriend == '') {
+        let message = document.getElementById("message");
+        message.innerHTML = 'Por favor, insira um nome';
+    } else {
+        friends.push(getFriend);
+        console.log(friends);
+        clearField();
+    }
+
+}
+
+function clearField() {
+    document.getElementById("friend").value = '';
+    document.getElementById("message").innerText = '';
+}
